b4d4a74d086bf43105129fb9f35c16f6beed3372
Vivek Prakash <vivekprakash27.iitkgp@gmail.com>
2024-08-29 00:26:00 +0530

    commit 13

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 const app = require('./app');
 
 // MongoDB database connection
